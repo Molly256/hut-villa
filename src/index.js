ed78959ddefa7c3ea695c403b912877dbcbaaa67
maureennakiyingi <maureennakiyingi50@gmail.com>
2026-05-26 05:49:39 -0700

    Fix build and setup KV API routes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,9 +10,6 @@ root.render(
     React.createElement(App, null)
   )
 );
-
-// Disable service worker since you don't need offline
-serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
